d26d442df7983783e249c8f7e5b2295b4913a863
Gabriele Miano <miano.gabriele@gmail.com>
2026-06-14 20:59:02 +0200

    feat: add redirect to swagger

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(requestLogger);
 
+app.get('/', (_req, res) => {
+  res.redirect('/api-docs');
+});
+
 app.get('/health', (_req, res) => {
   res.json({ status: 'ok' });
 });
