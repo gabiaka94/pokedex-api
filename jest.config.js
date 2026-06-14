6287f0cbe33119aa94ec4e2aa05ad0310a90923f
Gabriele Miano <miano.gabriele@gmail.com>
2026-06-14 16:56:33 +0200

    chore: fix jest configuration

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/tests'],
-  testMatch: ['**/*.test.ts'],
-  collectCoverageFrom: ['src/**/*.ts', '!src/server.ts'],
-};
